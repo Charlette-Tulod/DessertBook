11dd3c3aa86bee8ef3356b0507146a3160f08bb6
Charlette Tulod <charlettevibar@gmail.com>
2024-05-14 11:21:08 +0800

    Add not found page

---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <div className="text-center flex flex-col justify-center items-center h-96">
+      <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
+      <p className="text-xl mb-5">This page does not exist</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
