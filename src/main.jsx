24d749b62e78a07e89008077ed747b8fc7b5a536
Charlette Tulod <charlettevibar@gmail.com>
2024-05-14 13:02:30 +0800

    Wrap RecipeContext with the App

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import RecipeContextProvider from './context/RecipeContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <RecipeContextProvider>
+        <App />
+      </RecipeContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
