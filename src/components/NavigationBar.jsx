ea6deade0b64504659970af2c0751c0fa9b23da6
Charlette Tulod <charlettevibar@gmail.com>
2024-05-21 21:09:40 +0800

    Add mobile view

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,53 +1,37 @@
-// import React from 'react';
-// import { NavLink } from 'react-router-dom';
-// import Logo from '../assets/logo.png';
-
-// const NavigationBar = () => {
-//   const activationClass = ({ isActive }) =>
-//     isActive
-//       ? 'bg-darkerrose  hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-//       : ' hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
-
-//   return (
-//     <nav className="bg-lightrose border-b border-rose">
-//       <div className=" sm:px-6 lg:px-8">
-//         <div className="flex h-20 items-center justify-between">
-//           <div className="flex flex-1 items-center justify-center ">
-//             <NavLink className="" to="/">
-//               <img className="h-20 w-auto" src={Logo} alt="Dessert Book" />
-//             </NavLink>
-//             <div className="md:ml-auto">
-//               <div className="flex space-x-2">
-//                 <NavLink to="/" className={activationClass}>
-//                   Home
-//                 </NavLink>
-//                 <NavLink to="/recipes" className={activationClass}>
-//                   Desserts
-//                 </NavLink>
-//                 <NavLink to="/addrecipe" className={activationClass}>
-//                   Add Recipe
-//                 </NavLink>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default NavigationBar;
-
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { AppBar, Toolbar, Button } from '@mui/material';
+import { AppBar, Toolbar, Box, useMediaQuery, useTheme, Drawer, IconButton } from '@mui/material';
+import { IoMdMenu } from 'react-icons/io';
 import Logo from '../assets/logo.png';
 
 const NavigationBar = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
   const activationClass = ({ isActive }) =>
     isActive
       ? 'bg-darkerrose  hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
       : ' hover:bg-gray-900 hover:text-white rounded-md px-3 py-2';
+
+  const renderNavLinks = () => (
+    <>
+      <NavLink to="/" className={activationClass}>
+        Home
+      </NavLink>
+      <NavLink to="/recipes" className={activationClass}>
+        Desserts
+      </NavLink>
+      <NavLink to="/addrecipe" className={activationClass}>
+        Add Recipe
+      </NavLink>
+    </>
+  );
+
   return (
     <AppBar
       position="static"
@@ -57,16 +41,30 @@ const NavigationBar = () => {
         <NavLink to="/" style={{ textDecoration: 'none' }}>
           <img src={Logo} alt="Dessert Book" style={{ height: '60px', marginRight: '16px' }} />
         </NavLink>
-        <div style={{ flexGrow: 1 }}></div>
-        <NavLink to="/" className={activationClass}>
-          Home
-        </NavLink>
-        <NavLink to="/recipes" className={activationClass}>
-          Desserts
-        </NavLink>
-        <NavLink to="/addrecipe" className={activationClass}>
-          Add Recipe
-        </NavLink>
+        <Box sx={{ marginLeft: 'auto', display: { xs: 'none', md: 'flex' } }}>
+          <div style={{ flexGrow: 1 }}></div>
+          {renderNavLinks()}
+        </Box>
+        {isMobile && (
+          <>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ ml: 'auto' }}
+              onClick={handleDrawerToggle}
+            >
+              <IoMdMenu />
+            </IconButton>
+            <Drawer anchor="right" open={drawerOpen} onClose={handleDrawerToggle}>
+              <Box sx={{ p: 3 }}>
+                <div style={{ flexGrow: 1 }}></div>
+                {renderNavLinks()}
+              </Box>
+            </Drawer>
+          </>
+        )}
       </Toolbar>
     </AppBar>
   );
